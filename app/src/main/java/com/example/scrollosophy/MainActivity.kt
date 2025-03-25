365f2160c5fc5acd442c1be1920ee93740cd4e2d
Jannis Milz <67200746+jannismilz@users.noreply.github.com>
2025-03-25 08:33:41 +0100

    Remove unused imports

--- app/src/main/java/com/example/scrollosophy/MainActivity.kt
+++ app/src/main/java/com/example/scrollosophy/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.scrollosophy
 import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
-import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
